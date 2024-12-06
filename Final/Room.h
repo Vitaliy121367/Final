3a91652ca8c44b5a3f44fa5aec02481b713990fe
Vitaliy <qw929496@gmail.com>
2024-12-06 19:19:16 +0200

    Room

--- Final/Room.h
+++ Final/Room.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <iostream>
-#include <string>
 #include <fstream>
 #include "Date.h"
 using namespace std;
@@ -11,6 +10,27 @@ namespace Rooms {
 		int num;
 		int number;
 		Date race;
+		int days;
+		double priceDay;
+		bool exitToTheSea;
+	public:
+		Room();
+		Room(int identifier, int num, int number, Date race, int days, double priceDay, bool exitToTheSea);
+		virtual ~Room();
 
+		int getIdentifier()const;
+		int getNum()const;
+		int getNumber()const;
+		Date getRace()const;
+		int getDays()const;
+		double getPriceDay()const;
+		bool getExitToTheSea()const;
+
+		void setDays(int days);
+		void setPriceDay(bool exitToTheSea);
+
+		virtual void showInfo()const = 0;
+		virtual void showFullPrice()const = 0;
+		virtual void load(ifstream& file) = 0;
 	};
 }
